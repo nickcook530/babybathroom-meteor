4e1d647c3b967ec95c2eced32fffbd5b0093c27a
Nicholas Cook <nickcook530@gmail.com>
2018-01-07 22:30:37 -0500

    first commit of project

---
+++ both/main.js
@@ -0,0 +1,25 @@
+// ---------- Startup ----------- //
+Bathrooms = new Mongo.Collection('bathrooms');
+
+
+
+// ---------- Iron Routing ----------- //
+// Router.configure({
+//     // options that apply to all routes in the project
+//     layoutTemplate: 'masterLayout',
+//     notFoundTemplate: 'notFound',
+//     loadingTemplate: 'loading',
+// });
+
+// Router.route('/', {
+//     // options for the route
+//     name: 'home', //allows us to code href to this template
+//     template: 'home',
+// });
+
+// // seems to be an issue routing the google maps template
+// Router.route('/map', {
+//     // options for the route
+//     name: 'map', //allows us to code href to this template
+//     template: 'map',
+// });
